3c04e9017ebc36f1364dad5caa2a52ca79061894
Parker Lillywhite <parkerlillywhite@parkers-mbp.lan>
2021-09-28 14:46:17 -0600

    First commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Luke sucks lmao</h1>
+        <h2>testing only</h2>
+      </div>
+    );
+  }
+}
